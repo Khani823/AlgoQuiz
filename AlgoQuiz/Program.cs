f9d43d6d57b151f53a5ccf1fc07801f9bf1f6614
Limdeokhan <limdh001@gmail.com>
2023-12-14 09:22:10 +0900

    quiz 19 20
    19번 20번 풀었음

--- AlgoQuiz/Program.cs
+++ AlgoQuiz/Program.cs
@@ -24,6 +24,43 @@
             return answer;
         }
 
+        //19. 정수 제곱근 판별
+
+        public long solution19(long n)
+        {
+            long answer = 0;
+            double x = Math.Sqrt(n);
+            if (x % 1 == 0)
+            {
+                answer = ((long)x + 1) * ((long)x + 1);
+            }
+            else
+                answer = -1;
+
+            return answer;
+        }
+
+        //20. 정수 내림차순으로 배열하기
+        public long solution20(long n)
+        {
+            long answer = 0;
+            // n을 곧장 char형태로 변환해서 새로운 char 배열에 저장함
+            char[] temp = n.ToString().ToCharArray();
+
+            // 배열을 오름차순으로 정렬함
+            Array.Sort(temp);
+
+            // 배열을 뒤집는다 = 내림차순으로 바뀜
+            Array.Reverse(temp);
+
+            // temp배열을 문자열로 만든뒤에 long형태로 변환한다.
+            // long이므로 ToInt32가 아니라 64다. 헷갈린다면 그냥 long.Parse를 쓰자.
+            answer = Convert.ToInt64(new string(temp));
+
+            // 정답을 리턴함
+            return answer;
+        }
+
         // 21. 하샤드의 수
         public bool solution21(int x)
         {
